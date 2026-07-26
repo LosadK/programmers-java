004559b392175058c7383f403e450b8e73bb76cb
LosadK <130125143+LosadK@users.noreply.github.com>
2026-07-27 07:59:00 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        final int YEAR = 2022;
+        
+        answer = (YEAR - age) + 1;
+            
+        return answer;
+    }
+}
