b173dcad411118f15f4b153a9fad7b8f370a45c1
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-04 08:14:50 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.68 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,15 +1,44 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = {};
         
         int length = num_list.length;
-        int lastIndex = length - 1;
         answer = new int[length];
+        
+        // 내 풀이
+        /*
+        
+        int lastIndex = length - 1;
         
         for (int i = 0; i < length; i ++) {
             answer[i] = num_list[lastIndex];
             lastIndex--;
         }
+        */
+        
+        
+        // 다른 풀이1
+        List<Integer> list = Arrays.stream(num_list)
+            .boxed()
+            .collect(Collectors.toList());
+        
+        Collections.reverse(list);
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+        // 다른 풀이2
+        /*
+        answer = LongStream.range(1, length + 1)
+            .mapToInt(i -> num_list[(int) (length - i)])
+            .toArray();
+        */
+        
         
         return answer;
     }
