f2106c32f33124b434ba634e5fdda23027da3a07
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-04 23:06:27 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        // 내 풀이
+        int[] answer = new int[num2 - num1 + 1];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if (i >= num1 && i <= num2) {
+                answer[i - num1] = numbers[i];
+            }
+        }
+        
+        return answer;
+    }
+}
