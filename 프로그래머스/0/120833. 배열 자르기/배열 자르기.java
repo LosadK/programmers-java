200cb8e3a878d8c0f53fd8e958bfd96bf7df046b
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-04 23:13:49 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,13 +1,29 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        // 내 풀이
         int[] answer = new int[num2 - num1 + 1];
         
+        // 내 풀이
         for (int i = 0; i < numbers.length; i++) {
             if (i >= num1 && i <= num2) {
                 answer[i - num1] = numbers[i];
             }
         }
+        
+        
+        // 다른 풀이1
+        /*
+        answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
+        */
+        
+        
+        // 다른 풀이2
+        /*
+        answer = IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
+        */
+        
         
         return answer;
     }
