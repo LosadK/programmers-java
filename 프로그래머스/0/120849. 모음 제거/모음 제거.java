04f3643c4b07d9564d4c7b0351b70bb01f84cc2a
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-06 16:11:50 +0900

    [level 0] Title: 모음 제거, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,14 +2,31 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        // 내 풀이
         final String[] VOWELS = {"a", "e", "i", "o", "u"};
         
+        // 내 풀이
         for (String v : VOWELS) {
             my_string = my_string.replace(v, "");
         }
         
+        
+        // 다른 풀이1
+        /*
+        my_string = my_string.replaceAll("[aeiou]", "");
+        */
+        
+        
+        // 다른 풀이2
+        /*
+        for (String v : VOWELS) {
+            if (my_string.contains(v)) {
+                my_string = my_string.replace(v, "");
+            }
+        }
+        */
+        
         answer = my_string;
+        
         
         return answer;
     }
