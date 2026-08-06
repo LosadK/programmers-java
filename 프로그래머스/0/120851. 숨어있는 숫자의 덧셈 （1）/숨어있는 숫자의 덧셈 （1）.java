d0dd896065a693df2cee8e1c97907dbc365b5429
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-06 16:44:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.89 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
@@ -12,6 +14,23 @@ class Solution {
         }
         
         
+        // 다른 풀이1
+        /*
+        answer = Arrays.stream(my_string.split(""))
+            .filter(str -> str.matches("[0-9]"))
+            .mapToInt(Integer::parseInt)
+            .sum();
+        */
+        
+        
+        // 다른 풀이2
+        /*
+        answer = my_string.chars()  // 문자열의 각 문자를 IntStream 으로 만듦
+            .filter(Character::isDigit) // = .filter(c -> Character.isDigit(c))
+            .map(Character::getNumericValue)    // = .map(c -> Character.getNumericValue(c))
+            .sum();
+        */
+
         
         return answer;
     }
