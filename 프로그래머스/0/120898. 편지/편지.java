6e92eb4d4b7294e8753ca800070a424acde7a302
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-07 14:27:34 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        final int CHAR_SIZE = 2;
+        
+        answer = message.length() * CHAR_SIZE;
+        
+        return answer;
+    }
+}
