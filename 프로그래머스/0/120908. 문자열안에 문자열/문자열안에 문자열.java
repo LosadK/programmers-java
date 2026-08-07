e5f2c4d9d2cd87eef2b657640b9c9da13c98fc23
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-07 16:46:36 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,9 +1,31 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
+        int answer = 2;
 
         // 내 풀이
         answer = str1.contains(str2) ? 1 : 2;
+        
+        
+        // 다른 풀이
+        /*
+        for (int i = 0; i < str1.length() - str2.length(); i++) {
+            boolean isMatch = true;
+            
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    isMatch = false;
+                    break;
+                }
+            }
+            
+            if (isMatch) {
+                answer = 1;
+                return answer;
+            }
+                
+        }
+        */
+        
         
         return answer;
     }
