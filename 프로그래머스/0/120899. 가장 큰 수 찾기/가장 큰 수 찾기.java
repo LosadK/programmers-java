575708d84d41f05877e499a275e0b5f9a294a50a
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-07 14:54:21 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,19 +1,33 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = {};
         
-        // 내 풀이
         int max = 0;
-        int max_index=  0;
-
+        int max_index = array[0];
+        
+        // 내 풀이
         for (int i = 0; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
                 max_index = i;
             }
         }
-
+        
         answer = new int[]{max, max_index};
+        
+        
+        // 다른 풀이
+        /*
+        max_index = IntStream.range(0, array.length)
+            .reduce((i, j) -> array[i] > array[j] ? i : j)
+            .getAsInt();
+        
+
+        answer = new int[]{array[max_index], max_index};
+        */
+        
         
         return answer;
     }
