a94013295ec6c55ef6371bd27dee8a592337e66d
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-10 16:33:12 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.08 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -4,9 +4,30 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
         
-        // 내 풀이1
-        array = Arrays.stream(array).sorted().toArray();
-        answer = array[array.length / 2];        
+        // 내 풀이
+        // array = Arrays.stream(array).sorted().toArray();
+        // answer = array[array.length / 2];
+        
+
+        // 다른 풀이
+        
+        for (int i = 0; i < array.length -1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        
+        answer = array[array.length / 2];
+        
+        
         
         return answer;
     }
