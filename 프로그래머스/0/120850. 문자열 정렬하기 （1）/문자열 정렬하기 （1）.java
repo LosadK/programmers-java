5bfe88dac5710377de028ea7f691584784525d3d
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-11 16:59:55 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 5.44 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = {};
@@ -8,6 +10,16 @@ class Solution {
             .map(c -> c - '0')
             .sorted()
             .toArray();
+        
+        
+        // 다른 풀이
+        /*
+        answer = Arrays.stream(my_string.split(""))
+                .filter(s -> s.matches("[0-9]"))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        */
         
         
         return answer;
