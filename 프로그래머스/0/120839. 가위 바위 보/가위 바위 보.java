8f1d79cad3ee891bd936c1797b24b6140f5b9de7
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-13 09:09:27 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -2,13 +2,15 @@ class Solution {
     public String solution(String rsp) {
         String answer = "";
         
-        // 내 풀이
         final String SCISSORS = "2";
         final String ROCK = "0";
         final String PAPER = "5";
 
         StringBuilder sb = new StringBuilder();
 
+        
+        // 내 풀이
+        /*
         for (String s : rsp.split("")) {
             switch (s) {
                 case SCISSORS:
@@ -23,6 +25,18 @@ class Solution {
                 default:
                     sb.append("");
             }
+        }
+        */
+        
+        
+        // Java 14 이상일 경우
+        for (char c : rsp.toCharArray()) {
+            sb.append(switch (c) {
+                case '2' -> ROCK;
+                case '0' -> PAPER;
+                case '5' -> SCISSORS;
+                default -> c;
+            });
         }
 
         answer = sb.toString();
