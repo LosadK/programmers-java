001cecb1d28681dcc87a2f71e8b6ef2154a90995
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-14 16:56:22 +0900

    [level 0] Title: 암호 해독, Time: 0.09 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -3,6 +3,7 @@ class Solution {
         String answer = "";
         
         // 내 풀이
+        /*
         String[] cipher_arr = cipher.split("");
         StringBuilder sb = new StringBuilder();
         
@@ -13,7 +14,20 @@ class Solution {
                 sb.append(cipher_arr[i]);
             }
         }
-
+        
+        answer = sb.toString();
+        */
+        
+        
+        // 다른 풀이1
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < cipher.length(); i++) {
+            if ((i + 1) % code == 0) {
+                sb.append(cipher.charAt(i));
+            }
+        }
+        
         answer = sb.toString();
         
         
