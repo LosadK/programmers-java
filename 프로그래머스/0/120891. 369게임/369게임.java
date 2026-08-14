48a9800ca8d617e4e8feeb91495c5523349db6a7
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-14 15:34:41 +0900

    [level 0] Title: 369게임, Time: 1.57 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -5,10 +5,32 @@ class Solution {
         int answer = 0;
         
         // 내 풀이
+        /*
         answer = (int) Arrays.stream(String.valueOf(order).split(""))
                 .mapToInt(Integer::parseInt)
                 .filter(n -> n != 0 && (n % 3 == 0))
                 .count();
+        */
+        
+        
+        //다른 풀이1
+        answer = (int) String.valueOf(order).chars()
+                .filter(c -> c == '3' || c == '6' || c == '9')
+                .count();
+        
+        
+        // 다른 풀이2
+        /*
+        while (order > 0) {
+            int remain = order % 10;
+
+            if (remain > 0 && remain % 3 == 0) {
+                answer++;
+            }
+
+            order /= 10;
+        }
+        */
         
         
         return answer;
