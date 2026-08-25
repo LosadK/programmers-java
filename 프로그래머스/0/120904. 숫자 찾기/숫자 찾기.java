98dbd9ff0774fded54a7530b82c09c8b7d729e7d
LosadK <130125143+LosadK@users.noreply.github.com>
2026-08-25 09:50:10 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,8 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int num, int k) {
         int answer = 0;
         
         // 내 풀이
+        /*
         String[] num_arr = String.valueOf(num).split("");
 
         answer = -1;
@@ -13,6 +16,36 @@ class Solution {
                 break;
             }
         }
+        */
+        
+        
+        // 다른 풀이1
+        /*
+        String[] num_arr = String.valueOf(num).split("");
+
+        answer = IntStream.range(0, num_arr.length)
+                .filter(i -> k == Integer.parseInt(num_arr[i]))
+                .map(i -> i + 1)
+                .findFirst()
+                .orElse(-1);
+        */
+        
+        
+        // 다른 풀이2
+        /*
+        String num_str = String.valueOf(num);
+
+        answer = IntStream.range(0, num_str.length())
+                .filter(i -> num_str.charAt(i) - '0' == k)
+                .map(i -> i + 1)
+                .findFirst()
+                .orElse(-1);
+        */
+        
+        
+        // 다른 풀이3
+        int index = String.valueOf(num).indexOf(String.valueOf(k));
+        answer = index == -1 ? -1 : index + 1;
         
         
         return answer;
