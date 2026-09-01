82fa41731b7981aae220a5ceb250ca6ffb71234c
LosadK <130125143+LosadK@users.noreply.github.com>
2026-09-01 16:23:12 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -3,6 +3,7 @@ class Solution {
         int[] answer = {};
         
         // 내 풀이
+        /*
         switch (direction) {
             case "left":
                 for (int i = 0; i < numbers.length; i++) {
@@ -12,6 +13,7 @@ class Solution {
 
                     int prev_value = numbers[i];
                     int next_value = numbers[i + 1];
+                    
                     numbers[i + 1] = prev_value;
                     numbers[i] = next_value;
                 }
@@ -31,8 +33,42 @@ class Solution {
                 break;
         }
 
-
-        answer = numbers;        
+        answer = numbers;
+        */
+        
+        
+        // 다른 풀이1
+        /*
+        answer = new int[numbers.length];
+        
+        if (direction.equals("right")) {
+            answer[0] = numbers[numbers.length - 1];
+            
+            for (int i = 0; i < numbers.length - 1; i++) {
+                answer[i + 1] = numbers[i];
+            }
+        } else {
+            answer[numbers.length - 1] = numbers[0];
+            
+            for (int i = 1; i < numbers.length; i++) {
+                answer[i - 1] = numbers[i];
+            }
+        }
+        */
+        
+        
+        // 다른 풀이2
+        answer = new int[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if (direction.equals("right")) {
+                answer[(i + 1) % numbers.length] = numbers[i];
+            } else {
+                answer[i] = numbers[(i + 1) % numbers.length];
+            }
+        }
+        
+        
         
         return answer;
     }
