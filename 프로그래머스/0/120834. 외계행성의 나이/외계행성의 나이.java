219caf3667922b9ad26951a0f4e37ed6d6873fcd
LosadK <130125143+LosadK@users.noreply.github.com>
2026-09-08 08:38:31 +0900

    [level 0] Title: 외계행성의 나이, Time: 3.80 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -5,11 +5,20 @@ class Solution {
         String answer = "";
         
         // 내 풀이
+        /*
         final int DIFF_ALPHABET_TO_NUMBER = 49;
         
         answer = String.valueOf(age).chars()
                 .mapToObj(i -> String.valueOf((char) (i + DIFF_ALPHABET_TO_NUMBER)))
                 .collect(Collectors.joining());
+        */
+        
+        
+        // 다른 풀이
+        answer = String.valueOf(age).chars()
+                .mapToObj(i -> String.valueOf((char) (i - '0' + 'a')))
+                .collect(Collectors.joining());
+        
         
         return answer;
     }
